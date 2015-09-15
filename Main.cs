edb024edfa648af7567ed7603a5d67850beb8654
Sean Lao <slao258@gmail.com>
2015-09-15 08:46:13 -0700

    finish 11.3

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ namespace epi
     {
         public static void Main(string[] args)
         {
-            P10_10.RunTests();
+            P11_3.RunTests();
         }
     }
 }
---
+++ P11_3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace epi
+{
+    public class P11_3
+    {
+        public static int[] Sort(int[] arr, int k)
+        {
+            int[] result = new int[arr.Length];
+            int resultIndex = 0;
+            BinaryHeap<int> heap = new BinaryHeap<int>();
+
+            // add k items into heap
+            for (int i = 0; i < k && i < arr.Length; ++i)
+            {
+                heap.Insert(arr [i]); 
+            }
+
+            // add and remove
+            for (int i = k; i < arr.Length; ++i)
+            {
+                heap.Insert(arr [i]);
+                result [resultIndex++] = heap.RemoveRoot();
+            }
+
+            // remove remaining
+            while (heap.Count > 0)
+                result [resultIndex++] = heap.RemoveRoot();
+
+            return result;
+        }
+
+        public static void RunTests()
+        {
+            Print(Sort(new int[] { 1, 3, 2, 5, 4, 6, 8, 7 }, 1));
+            Print(Sort(new int[] { 5, 3, 1, 2, 4, 8, 6, 7 }, 4));
+        }
+
+        private static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write(arr [i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
