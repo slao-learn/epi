c38e42fdb5b9346d2a81f3eb7251dac612c910e6
Sean Lao <slao258@gmail.com>
2015-09-25 06:44:30 -0700

    finish 6.9

--- Main.cs
+++ Main.cs
@@ -8,7 +8,7 @@ namespace epi
     {
         public static void Main(string[] args)
         {
-            P6_6.RunTests();
+            P6_9.RunTests();
         }
     }
 }
---
+++ P6_9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace epi
+{
+    public class P6_9
+    {
+        public static List<int> EnumeratePrimes(int n)
+        {
+            List<int> l = new List<int>();
+            bool[] p = new bool[n + 1];
+            for (int i = 2; i < p.Length; ++i)
+            {
+                if (!p [i]) // is prime
+                {
+                    l.Add(i);
+                
+                    for (int j = i * i; j < p.Length; j += i)
+                    {
+                        p [j] = true;
+                    }
+                }
+            }
+            return l;
+        }
+
+        public static void RunTests()
+        {
+            Print(EnumeratePrimes(18));
+            Print(EnumeratePrimes(100));
+        }
+
+        private static void Print(List<int> l)
+        {
+            foreach (var i in l)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
